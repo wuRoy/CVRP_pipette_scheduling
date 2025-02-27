9c0d5d4afa0c1221c1bf7667e2aaf11782bd497c
runzhong <18309862+rogerwwww@users.noreply.github.com>
2025-02-27 00:56:07 -0500

    fix inconsistent results

--- src/QAP_solver.py
+++ src/QAP_solver.py
@@ -5,13 +5,19 @@ from src.ortools_solver import CVRP_solver
 pygm.set_backend('numpy') # set default backend for pygmtools
 np.random.seed(42) # fix random seed
 
-def calculate_S_E(jobs):
+def calculate_S_E(jobs, P=None):
     starting_point = np.zeros((jobs.shape[0], 96))
     ending_point = np.zeros((jobs.shape[0], 96))
+    ending_map = dict(np.argwhere(P if P is not None else np.eye(96)).tolist())
+    new_jobs = []
     for i in range(jobs.shape[0]):
-        starting_point[i,jobs[i,0]] = 1
-        ending_point[i,jobs[i,1]] = 1
-    return starting_point, ending_point
+        starting_point[i, jobs[i, 0]] = 1
+        ending_point[i, ending_map[jobs[i, 1]]] = 1
+        new_jobs.append([jobs[i, 0], ending_map[jobs[i, 1]]])
+    if P is None:
+        return starting_point, ending_point
+    else:
+        return starting_point, ending_point, np.array(new_jobs)
 
 def calculate_D_prime(D, S, E):
     # calculate D' = SDS^T + EDE^T
@@ -50,15 +56,15 @@ def calculate_T(sequences):
                 break
     return zeros
 
-def CVRP_QAP(jobs,iteration=5):
-    
+def CVRP_QAP(jobs, iteration=5, inner_cvrp_timewall=2, final_cvrp_timewall=10, ipfp_maxiter=50):
+
     D = np.ones((96,96))
     for i in range(96):
         for j in range(96):
             if i//8 == j//8:
                 if i-j == -1:
-                    D[i,j] = 0
-                    
+                    D[i, j] = 0
+
     output_P = np.eye(96)
     S, E = calculate_S_E(jobs)
     D_prime = calculate_D_prime(D, S, E)
@@ -67,10 +73,10 @@ def CVRP_QAP(jobs,iteration=5):
         # construct & update CVRP
         D_prime = np.vstack((np.zeros(D_prime.shape[0]), D_prime))
         D_prime = np.hstack((np.zeros((D_prime.shape[0], 1)), D_prime))
-        
+
         # solve CVRP
-        optimized_distance, recorder = CVRP_solver(D_prime.astype(np.int64), solving_time=2)
-        
+        optimized_distance, recorder = CVRP_solver(D_prime.astype(np.int64), solving_time=inner_cvrp_timewall)
+
         optimized_seuqnecess = get_optimized_sequence(recorder)
         t = calculate_T(optimized_seuqnecess)
         D_prime = D_prime[1:, 1:]
@@ -80,15 +86,14 @@ def CVRP_QAP(jobs,iteration=5):
         if best_cost > cost:
             best_cost = cost
             best_output_P = output_P
-            best_recorder = recorder
 
         # construct QAP
         A = np.dot(np.dot(E.T, t.T), E)
         B = D
         K = np.kron(1-B, A.T) # transform minimization into maximization
-        
+
         # solve QAP
-        P = pygm.ipfp((K + K.T), n1=96, n2=96, x0=np.eye(96)[None,:,:])
+        P = pygm.ipfp((K + K.T), n1=96, n2=96, x0=np.eye(96)[None,:,:], max_iter=ipfp_maxiter)
 
         # new_E = E * P
         new_E = np.dot(E, P)
@@ -99,24 +104,20 @@ def CVRP_QAP(jobs,iteration=5):
         if best_cost > cost:
             best_cost = cost
             best_output_P = output_P
-            best_recorder = recorder
 
         # update params
         D_prime = new_D_prime
         E = new_E
 
     # calculate best CVRP result with more solving time
-    S, E = calculate_S_E(jobs)
-    new_E = np.dot(E, best_output_P)
-    D_prime = calculate_D_prime(D, S, new_E)
+    S, E, new_jobs = calculate_S_E(jobs, best_output_P)
+    D_prime = calculate_D_prime(D, S, E)
     D_prime = np.vstack((np.zeros(D_prime.shape[0]), D_prime))
     D_prime = np.hstack((np.zeros((D_prime.shape[0], 1)), D_prime))
-    best_cost, best_recorder = CVRP_solver(D_prime.astype(np.int64))
+    best_cost, best_recorder = CVRP_solver(D_prime.astype(np.int64), solving_time=final_cvrp_timewall)
     print(f'solution cost={best_cost}')
 
     # transform to job id sequence
     best_recorder = get_optimized_sequence(best_recorder)
-    # best_sequence = best_recorder.flatten()
-    # best_sequence = best_sequence[best_sequence!=-1] -1
 
-    return best_cost, best_output_P, best_recorder
+    return best_cost, best_output_P, new_jobs, best_recorder
