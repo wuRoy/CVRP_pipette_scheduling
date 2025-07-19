bd213fd9e831efcd6f0059f320dacef057657b44
Roy Wu <roywu@Roys-MacBook-Pro.local>
2025-07-19 18:41:07 +0100

    update the package version and notebook to reproduce Figure 5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pipette_scheduling',
+    name='CVRP_pipette_scheduling',
     version='0.0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
